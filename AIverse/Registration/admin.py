fd4abd26d1bd101e82da4faf660291f8d2134991
Yashktyagi <147067258+Yashktyagi@users.noreply.github.com>
2025-02-11 21:08:05 +0530

    Update admin.py

--- AIverse/Registration/admin.py
+++ AIverse/Registration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(AIverseTeam)
+admin.site.register(AIverseMember)
+admin.site.register(DreambrushTeam)
+admin.site.register(DreambrushMember)
