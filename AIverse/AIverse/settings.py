5b52595293a198436325b76048f8cee1a179c305
Yashktyagi <yashktyagi18@gmail.com>
2025-03-15 17:20:12 +0530

    change settings

--- AIverse/AIverse/settings.py
+++ AIverse/AIverse/settings.py
@@ -116,11 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-import os
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # If static is in the root directory
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
