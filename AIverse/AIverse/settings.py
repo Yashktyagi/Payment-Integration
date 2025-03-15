956b00a3cb8ca9f0a279010cbfcb9c681d42fe8f
Yashktyagi <yashktyagi18@gmail.com>
2025-03-15 19:33:17 +0530

    change

--- AIverse/AIverse/settings.py
+++ AIverse/AIverse/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'AIverse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'MLM_EVENT',
+        'USER': 'postgres',
+        'PASSWORD' : 'jaishriram12',
+        'HOST' : 'localhost',
     }
 }
 
